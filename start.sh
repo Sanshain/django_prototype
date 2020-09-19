ecda4673e32eb9eebd43ee728804d0aae6f619c4
Sanshain <40761960+Sanshain@users.noreply.github.com>
2020-09-19 23:03:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ git clone https://github.com/Sanshain/django_prototype.git
 virtualenv __venv
 __venv/bin/activate
 pip install -r "django_prototype/requirements.txt"
+python manage.py migrate
 cd django_prototype/proto_app/static
 npm i
